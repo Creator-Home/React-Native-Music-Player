55321ae7f5f55fd06fd93a853199f6d954c4b138
McBryan <solomonmcbryan@gmail.com>
2020-06-10 18:20:01 +0000

    Homepage Done

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './src/screens/Homepage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Homepage/>
     </View>
   );
 }
@@ -12,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#000',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ src/screens/Homepage.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.icon__container}>
+          <FontAwesome5 name="headphones" size={100} color="white" />
+        </View>
+        <View style={styles.header__title}>
+          <Text style={styles.header__text}>Music Player</Text>
+        </View>
+        <View style={styles.touch}>
+          <TouchableOpacity style={styles.touch__touch} >
+            <Text style={styles.touch__text}>Get Started</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  icon__container: {
+    marginVertical: 80,
+  },
+  header__text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  header__title: {
+    marginTop: 30,
+  },
+  touch__text:{
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  touch__touch:{
+      borderBottomWidth: 10,
+      borderBottomColor: '#6800ad'
+  },
+  touch:{
+    marginVertical: 170,
+}
+});
+
+export default Homepage;
